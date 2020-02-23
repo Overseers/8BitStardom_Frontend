871d2b5c35c3c035aca5541d595d5b5d3a5e603c
Pertinate <nicholas@pertinate.info>
2020-02-23 15:09:50 -0600

    Getting prepared to theme it up
    Getting prepared to theme it up

--- src/Bedrock.jsx
+++ src/Bedrock.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Bedrock.css';
-import darkTheme from '@ant-design/dark-theme';
+import request from 'request-promise';
+import firebase from 'firebase';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 
 const { SubMenu } = Menu;
@@ -9,30 +10,52 @@ const { Header, Content, Footer, Sider } = Layout;
 export default class Bedrock extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            firebaseApp: firebase.initializeApp({
+                apiKey: 'AIzaSyCEJhDElHSuPLXF9_8CKCgmgCnjCw67Ktg',
+                authDomain: 'pertinate-info.firebaseapp.com',
+                databaseURL: 'https://pertinate-info.firebaseio.com',
+                projectId: 'pertinate-info',
+                storageBucket: 'pertinate-info.appspot.com',
+                messagingSenderId: '281043960559',
+                appId: '1:281043960559:web:a0fabe5ef2b2b5bba1111a',
+                measurementId: 'G-HYPX9WGJJ0',
+            }),
+            signInRequired: true,
+        };
     }
+
+    componentDidMount() {
+        this.state.firebaseApp
+            .auth()
+            .signInWithEmailAndPassword('ppertinate@gmail.com', '[Pertinate]18')
+            .then((result) => console.log(result))
+            .catch((error) => console.error(error));
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {}
 
     render() {
         return (
             <Layout>
-                <Header>
-                    <div className='logo'>
-                        <p style={{ textAlign: 'center', margin: '-15px', padding: '0px', color: 'white' }}>Pertinate</p>
-                    </div>
+                <Header className='header'>
+                    <div className='logo' />
                     <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
                         <Menu.Item key='1'>nav 1</Menu.Item>
                         <Menu.Item key='2'>nav 2</Menu.Item>
                         <Menu.Item key='3'>nav 3</Menu.Item>
                     </Menu>
                 </Header>
-                <Content style={{ padding: '0 50px', background: '#A4A4A4' }}>
+                <Content style={{ padding: '0 50px' }}>
                     <Breadcrumb style={{ margin: '16px 0' }}>
                         <Breadcrumb.Item>Home</Breadcrumb.Item>
                         <Breadcrumb.Item>List</Breadcrumb.Item>
                         <Breadcrumb.Item>App</Breadcrumb.Item>
                     </Breadcrumb>
-                    <Layout style={{ padding: '24px 0', background: '#cdcdcd' }}>
-                        <Sider width={200} style={{ background: '#cdcdcd' }}>
-                            <Menu mode='inline' defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} style={{ height: '100%' }} theme='dark'>
+                    <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                        <Sider width={200} style={{ background: '#fff' }}>
+                            <Menu mode='inline' defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} style={{ height: '100%' }}>
                                 <SubMenu
                                     key='sub1'
                                     title={
@@ -77,13 +100,7 @@ export default class Bedrock extends React.Component {
                         <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
                     </Layout>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Created by Pertinate (
-                    <a href='https://www.github.com/pertinate' target='_blank'>
-                        Nicholas Evans
-                    </a>
-                    )
-                </Footer>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
             </Layout>
         );
     }
